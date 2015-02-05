8b4caf89dec2a0d801c02dd4f45c350dbf01eca4
justin_wright <justin.wright@exponentialent.com>
2015-02-05 14:17:03 -0800

    Fixed up the Apptentive javascript interface since I started organizing it wrong previously

--- www/apptentive.js
+++ www/apptentive.js
@@ -1,27 +1,42 @@
-define(function (require) {
+// The following line is just an example to illistrate that you could define variables
+// here and then use them inside the scope of the 'Apptentive' object later on if that
+// feels appropriate for the situation
 
-	
-	var apptentive = {
-		init: function(api_key) {
-			
-            cordova.exec(function(err) {
-            		alert('Cordova Exec Success.' + api_key);
-        		}, function(err) {
-            		alert('App Exec Failed.');
-        		}, "ApptentiveBridge", "execute", [api_key]);
-        }
-        
-        event: function(event) {
-            cordova.exec(function(err) {
-            		alert('Cordova Exec Success.' + event);
-        		}, function(err) {
-            		alert('App Exec Failed.');
-        		}, "ApptentiveBridge", "execute", [event]);
-        }
-		
-	}
-	
-	return apptentive;
-	
+// var someLocalVars = [ 'varOne', 'varTwo'];
 
+var Apptentive = {
+
+    init: function(api_key) {
+        cordova.exec(
+            function(err) {
+        		alert('Cordova Exec Success.' + api_key);
+    		},
+            function(err) {
+        		alert('App Exec Failed.');
+    		},
+            "ApptentiveBridge",
+            "execute",
+            [api_key]
+        );
+    },
+
+    event: function(event) {
+        console.log("Apptentive.event", event)   // This will log to javascript console 
+
+        // Define success callback
+        var callback = function() {
+            alert('Cordova Exec Success.' + event); // This will pop up a browser alert (FYI: this will also pause javascript execution; not usually an issue)
+        };
+
+        // Define error callback
+        var errCallback = function(err) {
+            alert('App Exec Failed:', err);
+        };
+
+        // pass in all parameters to the cordova.exec to pass through to native code
+        cordova.exec(callback, errCallback, "ApptentiveBridge", "execute", [event]);
+    }
 }
+
+
+module.exports = Apptentive;
