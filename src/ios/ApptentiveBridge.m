858c0458b43c8fbfe194fc0e51e5e66243439440
Greg Bowman <greg.bowman@exponentialent.com>
2015-02-06 15:32:16 -0800

    Added execute to all the stubs, lots more iOS code

--- src/ios/ApptentiveBridge.m
+++ src/ios/ApptentiveBridge.m
@@ -4,52 +4,166 @@
 
 - (void)execute:(CDVInvokedUrlCommand*)command {
     NSString* callbackId = [command callbackId];
+    if ([command arguments].count == 0) {
+        [self sendFailureMessage:@"Insufficient arguments" callbackId:callbackId];
+        return;
+    }
     NSString* functionCall = [command argumentAtIndex:0];
+    NSLog([NSString stringWithFormat:@"Function call: %@",functionCall]);
     if ([functionCall isEqualToString:@"init"]) {
         [self initWithAPIKey:[command arguments] callBackString:callbackId];
     }
     else if ([functionCall isEqualToString:@"event"]) {
         [self logEvent:[command arguments] callBackString:callbackId];
     }
-    //    else if ([functionCall isEqualToString:@"getunreadmessagecount"]) {
-    //        [self getUnreadMessageCount:arguments callBackString:callbackId];
-    //    }
+    else if ([functionCall isEqualToString:@"engage"]) {
+        [self engage:[command arguments] callBackString:callbackId];
+    }
+    else if ([functionCall isEqualToString:@"getUnreadMessageCount"]) {
+        [self getUnreadMessageCount:callbackId];
+    }
+    else if ([functionCall isEqualToString:@"setProperty"]) {
+        [self setProperty:[command arguments] callBackString:callbackId];
+    }
+    else if ([functionCall isEqualToString:@"showMessageCenter"]) {
+        [self presentMessageCenterFromViewController];
+    }
+    else {
+        //command not recognized
+        [self sendFailureMessage:@"Command not recognized" callbackId:callbackId];
+    }
+    
 }
 
-- (void)initWithAPIKey:(NSArray*)arguments callBackString:(NSString*)callbackId {
-    NSLog(@"Calling initWithAPIKey");
-    NSString* apiKey = [arguments objectAtIndex:1];
-    [ATConnect sharedConnection].apiKey = apiKey;
-    
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:apiKey];
-    
-    [self.commandDelegate sendPluginResult:result callbackId:callbackId];
-}
-
-- (void)logEvent:(NSArray*)arguments callBackString:(NSString*)callbackId {
-    NSString* eventName = [arguments objectAtIndex:1];
-    [[ATConnect sharedConnection] engage:eventName fromViewController:self.viewController];
-    
-    NSString* msg = @"Logging event: ";
-    msg = [msg stringByAppendingString:eventName];
+- (void)sendSuccessMessage:(NSString*)msg callbackId:(NSString *)callbackId {
     CDVPluginResult* result = [CDVPluginResult
                                resultWithStatus:CDVCommandStatus_OK
                                messageAsString:msg];
-    
     [self.commandDelegate sendPluginResult:result callbackId:callbackId];
 }
-//
-//- (void) getUnreadMessageCount:(NSArray*)arguments callBackString:(NSString*)callbackId {
-//    NSUInteger unreadMessageCount = [[ATConnect sharedConnection] unreadMessageCount];
-//    NSString *messageCountAsString = [NSString stringWithFormat:@"%lu", (unsigned long)unreadMessageCount];
-//    CDVPluginResult* result = [CDVPluginResult
-//                               resultWithStatus:CDVCommandStatus_OK
-//                               messageAsString:messageCountAsString];
-//    
-//    [self success:result callbackId:callbackId];
-//}
+
+- (void)sendFailureMessage:(NSString*)error callbackId:(NSString *)callbackId {
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_ERROR
+                               messageAsString:error];
+    [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+}
+
+- (BOOL)parseBoolValue:(NSString*)value {
+    if ([value isEqualToString:@"true"] || [value isEqualToString:@"True"] || [value isEqualToString:@"yes"]) {
+        return YES;
+    }
+    else
+        return [value boolValue];
+}
+
+#pragma mark Initialization and Events
+- (void)initWithAPIKey:(NSArray*)arguments callBackString:(NSString*)callbackId {
+    if (arguments.count < 2) {
+        [self sendFailureMessage:@"Insufficient arguments - no apiKey" callbackId:callbackId];
+        return;
+    }
+    NSString* apiKey = [arguments objectAtIndex:1];
+    [ATConnect sharedConnection].apiKey = apiKey;
+}
+
+- (void)logEvent:(NSArray*)arguments callBackString:(NSString*)callbackId {
+    if (arguments.count < 2) {
+        [self sendFailureMessage:@"Insufficient arguments - no event name" callbackId:callbackId];
+        return;
+    }
+    NSString* eventName = [arguments objectAtIndex:1];
+    [[ATConnect sharedConnection] engage:eventName fromViewController:self.viewController];
+}
+
+- (void)engage:(NSArray*)arguments callBackString:(NSString*)callbackId {
+    NSString* eventLabel = [arguments objectAtIndex:1];
+    if([eventLabel isEqual:[NSNull null]]) {
+        [self sendFailureMessage:@"Insufficient arguments to call engage - label is null" callbackId:callbackId];
+        return;
+    }
+    if (arguments.count == 2) {
+        [[ATConnect sharedConnection] engage:eventLabel fromViewController:self.viewController];
+    }
+    else if (arguments.count == 3) {
+        //custom data needs to be parsed to NSDictionary - assuming json was passed
+        NSData* data = [[arguments objectAtIndex:2] dataUsingEncoding:NSUTF8StringEncoding];
+        NSError *localError = nil;
+        NSDictionary *customData = [NSJSONSerialization JSONObjectWithData:data options:0 error:&localError];
+        if (localError != nil) {
+            [self sendFailureMessage:@"Improperly formed json for engage custom data" callbackId:callbackId];
+            return;
+        }
+        [[ATConnect sharedConnection] engage:eventLabel withCustomData:customData fromViewController:self.viewController];
+    }
+    
+}
+
+
+#pragma mark Properties and Interface Customization
+
+- (void) setProperty:(NSArray*)arguments callBackString:(NSString*)callbackId {
+    if (arguments.count < 3) {
+        [self sendFailureMessage:@"Insufficient arguments for setting property" callbackId:callbackId];
+        return;
+    }
+    NSString *property_id = [arguments objectAtIndex:1];
+    NSString *value = [arguments objectAtIndex:2];
+    NSLog(@"Calling setProprty");
+    if ([property_id isEqualToString:@"appID"]) {
+        [ATConnect sharedConnection].appID = value;
+    }
+    else if ([property_id isEqualToString:@"showEmailField"]) {
+        [ATConnect sharedConnection].showEmailField = [self parseBoolValue:value];
+    }
+    else if ([property_id isEqualToString:@"customPlaceholderText"]) {
+        [ATConnect sharedConnection].customPlaceholderText = value;
+    }
+    else if ([property_id isEqualToString:@"useMessageCenter"]) {
+        //warning - useMessageCenter is deprecated
+        [ATConnect sharedConnection].useMessageCenter = [self parseBoolValue:value];
+    }
+    else if ([property_id isEqualToString:@"initiallyUseMessageCenter"]) {
+        [ATConnect sharedConnection].initiallyUseMessageCenter = [self parseBoolValue:value];
+    }
+    else if ([property_id isEqualToString:@"initialUserName"]) {
+        [ATConnect sharedConnection].initialUserName = value;
+    }
+    else if ([property_id isEqualToString:@"initialUserEmailAddress"]) {
+        [ATConnect sharedConnection].initialUserEmailAddress = value;
+    }
+    else if ([property_id isEqualToString:@"tintColor"]) {
+        NSArray *colorValues = [value componentsSeparatedByString:@","];
+        if (colorValues == nil || colorValues.count < 4) {
+            [self sendFailureMessage:@"TintColor not set with 4 comma separated float values" callbackId:callbackId];
+        }
+        UIColor* tintColor = [UIColor colorWithRed:[[colorValues objectAtIndex:0] floatValue] green:[[colorValues objectAtIndex:1] floatValue] blue:[[colorValues objectAtIndex:2] floatValue] alpha:[[colorValues objectAtIndex:3] floatValue]];
+        [ATConnect sharedConnection].tintColor = tintColor;
+    }
+    else {
+        [self sendFailureMessage:@"Property name not recognized" callbackId:callbackId];
+    }
+}
+
+#pragma mark Presenting UI
+
+- (void)presentMessageCenterFromViewController {
+    [[ATConnect sharedConnection] presentMessageCenterFromViewController:self.viewController];
+}
+
+- (void)presentMessageCenterFromViewControllerWithCustomData {
+//    [[ATConnect sharedConnection] presentMessageCenterFromViewController:self.viewController];
+}
+
+- (void) getUnreadMessageCount:(NSString*)callbackId {
+    NSUInteger unreadMessageCount = [[ATConnect sharedConnection] unreadMessageCount];
+    NSString *messageCountAsString = [NSString stringWithFormat:@"%lu", (unsigned long)unreadMessageCount];
+    [self sendSuccessMessage:messageCountAsString callbackId:callbackId];
+}
+
 
 
 @end
+
+
+
--- www/apptentive.js
+++ www/apptentive.js
@@ -1,4 +1,4 @@
-// The following line is just an example to illistrate that you could define variables
+cordova.define("com.cordova.apptentive.Apptentive", function(require, exports, module) { // The following line is just an example to illistrate that you could define variables
 // here and then use them inside the scope of the 'Apptentive' object later on if that
 // feels appropriate for the situation
 
@@ -45,133 +45,137 @@ var Apptentive = {
     },
 
     addAmazonSnsPushIntegration: function(successCallback, errorCallback, registrationId) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "addAmazonSnsPushIntegration", [registrationId]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["addAmazonSnsPushIntegration", registrationId]);
     },
 
     addCustomDeviceData: function(successCallback, errorCallback, key, value) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "addCustomDeviceData", [key, value]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["addCustomDeviceData", key, value]);
     },
 
     addCustomPersonData: function(successCallback, errorCallback, key, value) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "addCustomPersonData", [key, value]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["addCustomPersonData", key, value]);
     },
 
     addIntegration: function(successCallback, errorCallback, integration, config) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "addIntegration", [integration, config]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["addIntegration", integration, config]);
     },
 
     addParsePushIntegration: function(successCallback, errorCallback, deviceToken) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "addParsePushItegration", [deviceToken]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["addParsePushItegration", deviceToken]);
     },
 
     addUrbanAirshipPushIntegration: function(successCallback, errorCallback, appId) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "addUrbanAirshipPushIntegration", [appId]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["addUrbanAirshipPushIntegration", appId]);
     },
 
     engage: function(successCallback, errorCallback, eventId, customData) {
-        if( customData && customData typeof === 'object' ) {
-            cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "engage", [eventId, customData]);
+        if( customData && typeof customData === 'object' ) {
+            cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["engage", eventId, customData]);
         } else {
-            cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "engage", [eventId]);
+            alert(eventId);
+            cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["engage", eventId]);
         }
     },
 
     getUnreadMessageCount: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "getUnreadMessageCount", []);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["getUnreadMessageCount"]);
     },
 
     handleOpenedPushNotification: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "handleOpenedPushNotification", []);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["handleOpenedPushNotification"]);
     },
 
     isApptentivePushNotification: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "isApptentivePushNotification", []);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["isApptentivePushNotification"]);
     },
 
     onStart: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "onStart", []);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["onStart"]);
     },
 
     onStop: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "onStop", []);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["onStop"]);
     },
 
     putRatingProviderArg: function(successCallback, errorCallback, key, value) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "putRatingProviderArg", [key, value]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["putRatingProviderArg", key, value]);
     },
 
     removeCustomDeviceData: function(successCallback, errorCallback, key) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "removeCustomDeviceData", [key]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["removeCustomDeviceData", key]);
     },
 
     removeCustomPersonData: function(successCallback, errorCallback, key) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "removeCustomPersonData", [key]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["removeCustomPersonData", key]);
     },
 
     sendAttachmentFile: function(successCallback, errorCallback, uri, mimeType) {
         if(mimeType) {
-            cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "sendAttachmentFile", [uri, mimeType]);
+            cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["sendAttachmentFile", uri, mimeType]);
         } else {
-            cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "sendAttachmentFile", [uri]);
+            cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["sendAttachmentFile", uri]);
         }
     },
 
     sendAttachmentText: function(successCallback, errorCallback, text) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "sendAttachmentText", [text]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["sendAttachmentText", text]);
     },
 
     setCustomDeviceData: function(successCallback, errorCallback, customDeviceData) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "setCustomDeviceData", [customDeviceData]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["setCustomDeviceData", customDeviceData]);
     },
 
     setCustomPersonData: function(successCallback, errorCallback, customPersonData) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "setCustomPersonData", [customPersonData]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["setCustomPersonData", customPersonData]);
     },
 
     setInitialUserEmail: function(successCallback, errorCallback, email) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "setInitialUserEmail", [email]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["setInitialUserEmail", email]);
     },
 
     setInitialUserName: function(successCallback, errorCallback, name) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "setInitialUserName", [name]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["setInitialUserName", name]);
     },
 
     setOnSurveyFinishedListener: function(successCallback, errorCallback, listener) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "setOnSurveyFinishedListener", [listener]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["setOnSurveyFinishedListener", listener]);
     },
 
     setParsePushCallback: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "setParsePushCallback", []);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["setParsePushCallback"]);
     },
 
     setPendingPushNotification: function(successCallback, errorCallback, intent) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "setPendingPushNotification", [intent]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["setPendingPushNotification", intent]);
     },
 
     setProperty: function(success_callback, error_callback, property_id, value) {
-        cordova.exec(success_callback, error_callback, "ApptentiveBridge", "setProperty", [property_id, value]);
-    }
+        cordova.exec(success_callback, error_callback, "ApptentiveBridge", "execute", ["setProperty", property_id, value]);
+    },
 
     setRatingProvider: function(successCallback, errorCallback, ratingProvider) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "setRatingProvider", [ratingProvider]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["setRatingProvider", ratingProvider]);
     },
 
     setUnreadMessagesListener: function(successCallback, errorCallback, listener) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "setUnreadMessagesListener", [listener]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["setUnreadMessagesListener", listener]);
     },
 
     showMessageCenter: function(successCallback, errorCallback, customData) {
+        alert("show message center");
         if( customData ) {
-            cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "showMessageCenter", [customData]);
+            cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["showMessageCenter", customData]);
         } else {
-            cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "showMessageCenter", []);
+            cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["showMessageCenter"]);
         }
     },
 
     willShowInteraction: function(successCallback, errorCallback, event) {
-        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "setRatingProvider", [event]);
+        cordova.exec(successCallback, errorCallback, "ApptentiveBridge", "execute", ["setRatingProvider", event]);
     }
 }
 
 //window.Apptentive = Apptentive;
 module.exports = Apptentive;
+
+});
